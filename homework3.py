a62bff59da71399d0996e95912c86d02e04bde80
SergeiChun <GunneRX@yandex.ru>
2024-05-15 15:35:51 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = (input('Почему земля круглая?')) # значение строки с произвольным текстом
+print(len(my_string)) #длину символов введённого текста
+print(my_string.upper()) #в верхнем регистре
+print(my_string.lower()) #в нижнем регистре
+print(my_string.replace(' ','')) #удалив все пробелы
+print(my_string[0]) # первый символ строки
+print(my_string[-1])  # последний символ строки
